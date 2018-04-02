95905a88499385b7f8964dc35c354a6c684087d5
Lucas Cunico Kalinowski <lucas-cunico@livetouch.com.br>
2018-04-02 10:57:58 -0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Route} from 'react-router-dom';
+import Categories from './components/Categories';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Route exact path="/" component={Categories} />
+      </div>
+    );
+  }
+}
+
+export default App;
